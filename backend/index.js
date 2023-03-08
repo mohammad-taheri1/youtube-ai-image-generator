e9a5f4484f53f32753e02a282d734eaaef4da558
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-03-08 14:23:50 +0330

    finish backend

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,45 @@
-// const express = require("express");
-
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const configuration = new Configuration({
+   apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
 
 app.get("/", (req, res) => {
    return res.status(200).send("Server is up");
+});
+
+app.post("/generate", async (req, res) => {
+   const { prompt, size } = req.body;
+   //    validation
+   if (!prompt || !size) {
+      return res.status(400).send("Bad Request");
+   }
+   try {
+      const response = await openai.createImage({
+         prompt,
+         size,
+         n: 1,
+      });
+      const image_url = response.data.data[0].url;
+
+      return res.status(200).send({
+         src: image_url,
+      });
+   } catch (error) {
+      console.log(error);
+      return res.status(500).send({ error });
+   }
 });
 
 const port = process.env.PORT || 8200;
