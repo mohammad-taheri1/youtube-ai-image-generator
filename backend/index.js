ebd9b1096c2b996614b8b961c3765e3e0f4c6d11
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-03-08 14:07:04 +0330

    update

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+// const express = require("express");
+
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+app.get("/", (req, res) => {
+   return res.status(200).send("Server is up");
+});
+
+const port = process.env.PORT || 8200;
+
+app.listen(port, () => {
+   console.log(`Server is listening on port ${port}`);
+});
